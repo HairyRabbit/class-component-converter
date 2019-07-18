16cde24d9f603d8346f54a4275caefcc1536c513
Rabbit <yfhj1990@hotmail.com>
2019-07-18 14:56:33 +0800

    Update deploy script and add NPM_TOKEN

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,14 +8,19 @@ import genBinFile from 'bin-file-gen'
 const OUTPUT_DIRECTORY: string = `../libs`
 const PACKAGE_CONFIG: string = `../package.json`
 
-export default function main(): void {
-    execSync(`npm run build`)
-    const output: string = path.resolve(__dirname, OUTPUT_DIRECTORY)
-    fs.copyFileSync(
-            path.resolve(__dirname, PACKAGE_CONFIG), 
-            path.resolve(output, `package.json`)
+function copyFile(from: string, output: string) {
+    const fileName: string = path.basename(from)
+    return fs.copyFileSync(
+        path.resolve(__dirname, from), 
+        path.resolve(output, fileName)
     )
+}
+
+export default function main(): void {
+    const output: string = path.resolve(__dirname, OUTPUT_DIRECTORY)
+    copyFile(PACKAGE_CONFIG, output)
     genBinFile({ output })
+    execSync(`npm publish`, { cwd: output })
 }
 
 main()
